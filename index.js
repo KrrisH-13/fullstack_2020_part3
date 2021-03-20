e91a94db2239a9b23d5055159d5005fb07cf1ea1
Krishna <krishna.indi@gmail.com>
2021-03-20 16:13:57 +0200

    Solutions to Exercises 3.4 phonebook backend step 4   * added DELETE endpoint /api/persons/{id} to delete contact by id.

--- index.js
+++ index.js
@@ -53,6 +53,12 @@ app.get('/api/persons/:id',(request,response) => {
 });
 
 
+app.delete('/api/persons/:id', (request,response) => {
+    const id = Number(request.params.id);
+    phonebook = phonebook.filter((contact)=> contact.id !== id);
+    response.status(204).end();
+});
+
 app.listen(PORT, ()=>{
     console.log(`App running on port${PORT}`);
 });
