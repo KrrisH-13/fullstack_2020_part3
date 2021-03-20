1132c69d2484c4715d6d4ea890ecd41558abb5e2
Krishna <krishna.indi@gmail.com>
2021-03-20 16:06:20 +0200

    Solutions to Exercises 3.3 phonebook backend step 3   * added endpoint /api/persons/{id} to search for contact by id.   * 404 returned when no contact found.

--- index.js
+++ index.js
@@ -42,6 +42,16 @@ app.get('/info',(request, response)=>{
     response.send(infoPage);
 });
 
+app.get('/api/persons/:id',(request,response) => {
+    const id = Number(request.params.id);
+    const contact = phonebook.filter((person)=> person.id === id);
+
+    if(contact.length) return response.json(contact[0]);
+    // If the contact was not found, send a 404.
+    console.log(`Requested resource with ID ${id} was not found`);
+    response.status(404).end();
+});
+
 
 app.listen(PORT, ()=>{
     console.log(`App running on port${PORT}`);
